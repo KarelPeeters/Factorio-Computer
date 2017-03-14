9e482edce9234378b8956e63f7152c958b4c4919
flaghacker <karel.peeters.leuven@gmail.com>
2017-03-14 20:38:12 +0100

    Moved all lua code to separate resource package

--- src/main/java/com/flaghacker/factoriopc/circuits/LuaCompression.java
+++ src/main/java/com/flaghacker/factoriopc/circuits/LuaCompression.java
@@ -3,7 +3,10 @@ package com.flaghacker.factoriopc.circuits;
 import org.json.JSONObject;
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.JseBaseLib;
 import org.luaj.vm2.lib.jse.JsePlatform;
+
+import java.io.InputStream;
 
 public class LuaCompression
 {
@@ -13,6 +16,7 @@ public class LuaCompression
 	static
 	{
 		Globals globals = JsePlatform.standardGlobals();
+		globals.load(new PrefixLoader("/lua/"));
 
 		compressProgram = globals.loadfile("compress.lua");
 		decompressProgram = globals.loadfile("decompress.lua");
@@ -26,5 +30,21 @@ public class LuaCompression
 	public static JSONObject decompressString(String string)
 	{
 		return new JSONObject(decompressProgram.call(string).tojstring());
+	}
+
+	private static class PrefixLoader extends JseBaseLib
+	{
+		private String prefix;
+
+		public PrefixLoader(String prefix)
+		{
+			this.prefix = prefix;
+		}
+
+		@Override
+		public InputStream findResource(String filename)
+		{
+			return super.findResource(prefix + filename);
+		}
 	}
 }
